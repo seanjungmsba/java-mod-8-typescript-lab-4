f0a900b8275add19a59209214a78465fb7327e52
seanjungmsba <seanjeong1996@gmail.com>
2022-08-15 16:04:16 -0400

    Implemented a generic prepend() method that takes an array object

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-// your code here
+const button = document.querySelector("button") as HTMLButtonElement;
+const input1 = document.getElementById("spaceSize") as HTMLInputElement;
+const input2 = document.getElementById("stringValue") as HTMLInputElement;
+
+function prepend<T>(array: T[]) {
+    // https:// developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+    return " ".repeat(+array[0]) + array[1];
+}
+
+button!.addEventListener("click", function () {
+    console.log(prepend([input1!.value, input2!.value]));
+});
